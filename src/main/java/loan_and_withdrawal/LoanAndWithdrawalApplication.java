a5db2da40c60a37a90fe4db5f48407ab2f65fdf2
ashu.s <ashu40h@gmail.com>
2025-02-28 16:29:38 +0530

    first commit

---
+++ src/main/java/loan_and_withdrawal/LoanAndWithdrawalApplication.java
@@ -0,0 +1,13 @@
+package loan_and_withdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAndWithdrawalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAndWithdrawalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loan_and_withdrawal/LoanAndWithdrawalApplicationTests.java
@@ -0,0 +1,13 @@
+package loan_and_withdrawal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAndWithdrawalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
